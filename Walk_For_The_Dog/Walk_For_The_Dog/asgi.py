85d9cb6e47f23508cbd1e4b36cbf61ade5d8ce87
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-11-28 18:21:33 +0100

    Started setup. Everything is ready for preparing start Page

---
+++ Walk_For_The_Dog/Walk_For_The_Dog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Walk_For_The_Dog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Walk_For_The_Dog.settings')
+
+application = get_asgi_application()
---
+++ Walk_For_The_Dog/Walk_For_The_Dog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Walk_For_The_Dog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Walk_For_The_Dog.settings')
+
+application = get_wsgi_application()
