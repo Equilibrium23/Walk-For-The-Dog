dc31d2ef55ef56a2a88b04cfffd3de851011b75e
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-12-09 13:50:58 +0100

    add ALLOWED_HOSTS

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*u$!rvj#=)et2#cae0ohjc-jr%8!$#s5ei!r!$)*7rj3=)a&#s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['walk-for-the-dog.herokuapp.com','127.0.0.1']
 
 
 # Application definition
