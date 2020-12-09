3bfdb73d017f71433775aa1ca8b2f5a4a35118d0
Gabriela Leśniak <gabcia100@gmail.com>
2020-12-09 13:53:07 +0100

    whitenoise in requirements.txt

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'Walk_For_The_Dog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR + 'db.sqlite3',
     }
 }
 
