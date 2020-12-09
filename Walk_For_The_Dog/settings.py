5e1d34435acd3580fd767b3755a20db53c5b7a2e
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-12-09 13:29:21 +0100

    add import os

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,3 +123,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
