13489d30fbcb0dbc53393875d442058c876d0fed
Weronika <veroniqaa@LAPTOP-3FTT0ETC.localdomain>
2020-12-13 23:13:39 +0100

    update

--- register_and_login/models.py
+++ register_and_login/models.py
@@ -47,15 +47,6 @@ class Dog(models.Model):
 	def __str__(self):
 		return self.dog_name
 
-	def save(self, *args, **kwargs):
-		super(Dog, self).save(*args, **kwargs)
-
-		img=Image.open(self.image.path)
-		if img.height > 300 or img.width > 300:
-			output_size = (300, 300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
-
 
 class TimePeriod(models.Model):
 
