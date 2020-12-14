e11b574657d5add67980531ce45b83fee82c7c09
Gabriela Leśniak <gabcia100@gmail.com>
2020-12-14 09:26:55 +0100

    new requirements and settings

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -144,3 +145,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
