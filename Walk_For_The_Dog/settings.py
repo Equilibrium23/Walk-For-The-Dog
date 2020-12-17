a0b39ee8171e3b23e3f20b285296a12db5d53091
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-12-17 12:44:38 +0100

    add testing db

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
         'USER': 'idnrkogw',
         'PASSWORD': 'A0zqcSBOw9U_6z91ep6XQ_HhKmNBO6BR',
         'HOST': 'suleiman.db.elephantsql.com',
-        'PORT': '5432'
-        # 'TEST': {
-        #     'NAME': 'mytestdatabase',
-        # } #this may help with testing jj
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'idnrkogw',
+        } #this may help with testing jj
     }
 }
 
