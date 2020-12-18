9ed63abaec0c58cad5f337ce5c2ad804a218871b
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-12-18 15:24:14 +0100

    add tests

--- Walk_For_The_Dog/tests.py
+++ Walk_For_The_Dog/tests.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
 from django.urls import reverse, resolve
 
-from register_and_login.views import register, add_dog, update, change_ac_type
+from register_and_login.views import register, add_dog, update, change_ac_type, add_time_period
 from users.views import profile, CalendarView
+from start_page.views import start_page
+from django.contrib.auth import get_user_model
 
 
 class TestUrls(SimpleTestCase):
+    def test_start_page_is_resolved(self):
+        url = reverse('start_page')
+        self.assertEquals(resolve(url).func, start_page)
+
     def test_login_is_resolved(self):
         url = reverse('login')
         self.assertEquals(resolve(url).func.view_class, LoginView)
@@ -38,4 +44,84 @@ class TestUrls(SimpleTestCase):
     def test_change_ac_type_is_resolved(self):
         url = reverse('change_ac_type')
         self.assertEquals(resolve(url).func, change_ac_type)
+    
+    def test_change_ac_type_is_resolved(self):
+        url = reverse('add_time_period')
+        self.assertEquals(resolve(url).func, add_time_period)
 
+
+# # from django.contrib.auth import get_user_model
+# # from django.contrib.auth.models import User
+# register_data = {
+#             'csrfmiddlewaretoken':'TtGVEhVsVewJkhI9vRFE0rUus4KPhbRw8KCpz8ZPXu15S2Jp5uuihn31tGwPoZ5P',
+#             'username':'testing',
+#             'name':'testing',
+#             'email':'testing@testing.pl',
+#             'password1':'pite12345678',
+#             'password2':'pite12345678',
+#             'account_type':'1',
+#             'location':'Krakow',
+#             'image':'',
+#             'joining_date':'2020-12-18'
+#         }
+# class TestUrlResponse(TestCase):
+#     # def test_register_get_site_response(self):
+#     #     url = reverse('register')
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='get register request is invalid')
+    
+#     def test_register_post_data_response(self):
+#         url = reverse('register')
+#         response = self.client.post(url,register_data)
+#         self.assertEquals(response.status_code, 302, msg ='post register request is invalid')
+    
+#     # def test_get_login_response(self):
+#     #     url = reverse('login')
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='get login request is invalid')
+    
+#     # def test_post_login_response(self):
+#     #     url = reverse('login')
+#     #     login_data = {
+#     #         'csrfmiddlewaretoken':'TtGVEhVsVewJkhI9vRFE0rUus4KPhbRw8KCpz8ZPXu15S2Jp5uuihn31tGwPoZ5P',
+#     #         'username':'testing097',
+#     #         'password':'pite12345678'
+#     #     }
+#     #     response = self.client.post(url,login_data)
+#     #     self.assertEquals(response.status_code, 302, msg ='post login request is invalid')
+
+#     # def test_logout_response(self):
+#     #     url = reverse('logout')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='logout request is invalid')
+
+#     # def test_profile_response(self):
+#     #     url = reverse('profile')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='profile request is invalid')
+
+#     # def test_calendar_response(self):
+#     #     url = reverse('calendar')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='calendar request is invalid')
+
+#     # def test_add_dog_response(self):
+#     #     url = reverse('add_dog')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='add_dog request is invalid')
+
+#     # def test_add_update_response(self):
+#     #     url = reverse('update')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='update request is invalid')
+
+#     # def test_change_ac_type_response(self):
+#     #     url = reverse('change_ac_type')
+#     #     self.client.login(username = register_data['username'], password = register_data['password1'])
+#     #     response = self.client.get(url)
+#     #     self.assertEquals(response.status_code, 200, msg ='change_ac_type request is invalid')
--- Walk_For_The_Dog/urls.py
+++ Walk_For_The_Dog/urls.py
@@ -18,14 +18,14 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from register_and_login import views as register_and_login_views
 from users import views as users_views
+from start_page import views as start_page_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('start_page.urls')),
-
+    path('', start_page_views.start_page, name = "start_page"),
     #path('login/', register_and_login_views.login, name = "login"),
     path('login/', auth_views.LoginView.as_view(template_name='register_and_login/login.html'), name = "login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='register_and_login/logout.html'), name = "logout"),
@@ -40,7 +40,6 @@ urlpatterns = [
     path('change_ac_type/', register_and_login_views.change_ac_type, name = "change_ac_type"),
     path('add_time_period/', register_and_login_views.add_time_period, name = "add_time_period"),
     path('edit_dog_profile/', register_and_login_views.edit_dog_profile, name = "edit_dog_profile"),
-
 ]
 
 if settings.DEBUG:
