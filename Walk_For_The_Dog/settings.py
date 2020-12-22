32ce6cbbcaaae424a552ed470eb7a101bb78b120
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2020-12-22 12:42:05 +0100

    test new setup with testing

--- Walk_For_The_Dog/settings.py
+++ Walk_For_The_Dog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import django_heroku
  
 
@@ -80,20 +81,38 @@ WSGI_APPLICATION = 'Walk_For_The_Dog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'idnrkogw',
-        'USER': 'idnrkogw',
-        'PASSWORD': 'A0zqcSBOw9U_6z91ep6XQ_HhKmNBO6BR',
-        'HOST': 'suleiman.db.elephantsql.com',
-        'PORT': '5432',
-        'TEST': {
-            'NAME': 'idnrkogw',
-        } #this may help with testing jj
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'idnrkogw',
+            'USER': 'idnrkogw',
+            'PASSWORD': 'A0zqcSBOw9U_6z91ep6XQ_HhKmNBO6BR',
+            'HOST': 'suleiman.db.elephantsql.com',
+            'PORT': '5432',
+            # 'TEST': {
+            #     'NAME': 'idnrkogw',
+            # } #this may help with testing jj
+        }
+    }
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 
 
