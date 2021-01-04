a66541b711ca3d65856b6d83ff7f7231d8d2f625
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2021-01-04 16:16:09 +0100

    improve match

---
+++ users/matchPeople.py
@@ -0,0 +1,49 @@
+from .googleMapsUtils import check_location
+from register_and_login.models import Dog, Profile, TimePeriod, DogTime
+
+def check_dog_size(temp_user_dogs, helper_max_size_dog):
+    size_values = {'S':1,'M':2,'B':3}
+    match = False
+    matched_dogs = []
+    for temp_user_dog in temp_user_dogs:
+        dog_object = Dog.objects.get( id = temp_user_dog.dog_id )
+        if size_values[ dog_object.size ] <= size_values[helper_max_size_dog]:
+            matched_dogs.append(temp_user_dog)
+            match = True
+    return  ( matched_dogs, match )
+
+def check_time(helper, dogs):
+    helper_time_period = TimePeriod.objects.all().filter( person_id = helper.user_id ).filter(time_type = 'F') # to do person_id = helper.user_id -> person_id = helper.id
+    matched_dogs_and_time = {}
+    match = False
+    for helper_time in helper_time_period:
+        for dog in dogs:
+            dog_time_period = TimePeriod.objects.get( id = dog.time_period_id ) # czy trzeba sprawdzac czy ten czas jest typu F ?
+            if (helper_time.day == dog_time_period.day) and (helper_time.start_hour == dog_time_period.start_hour ) and (helper_time.end_hour == dog_time_period.end_hour):
+                matched_dogs_and_time[Dog.objects.get( id = dog.dog_id )] = dog_time_period 
+                match = True
+    return (matched_dogs_and_time,match)
+
+def matchUsers(request):
+    temp_user = Profile.objects.all().filter(user = request.user)
+    temp_user_dogs_need_walk = DogTime.objects.all().filter(owner_id = temp_user[0].id).filter(match = False)
+    helpers = Profile.objects.all().filter(account_type='H')
+    #####################################################################################################################
+    #match temp user with helpers by dog size
+    dict_of_helpers_dog_size = {} # { matched_helper : list of dogs which helper can walk with (from DogTime table) }
+    for helper in helpers:
+        match_by_dog_size = check_dog_size(temp_user_dogs_need_walk, helper.max_dog_size)
+        if match_by_dog_size[1] == True:
+            dict_of_helpers_dog_size[helper] = match_by_dog_size[0]
+    #####################################################################################################################
+    #match temp user with helpers by time
+    dict_of_helpers_time = {} # { matched_helper (from Profile table) : { dog (from Dog table) : time for walk (from TimePeriod table) }
+    for helper, dogs in dict_of_helpers_dog_size.items():
+        match_by_time = check_time(helper, dogs)
+        if match_by_time[1] == True:
+            dict_of_helpers_time[helper] = match_by_time[0]
+    #####################################################################################################################
+    #match temp user with helpers by distance 
+    list_of_helpers_distance = { helper:data for helper,data in dict_of_helpers_time.items() if check_location( temp_user[0].location, helper.location, helper.helping_radius) }
+    #####################################################################################################################
+    return list_of_helpers_distance
--- users/views.py
+++ users/views.py
@@ -6,103 +6,13 @@ from register_and_login.models import Dog, Profile
 @login_required
 def profile(request):
     context = { 'dogs' : Dog.objects.all().filter(owner_id=request.user.profile.id) }
-
     return render(request, 'users/profile.html', context) 
 
-import urllib.request
-import json
-
-def check_location(location_A,location_B,helping_radius):
-    url = '''https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={}&destinations={}&key={}'''.format(location_A,location_B,GOOGLE_MAP_KEY)
-    response = urllib.request.urlopen(url)
-    result = json.loads(response.read())
-    status = result['rows'][0]['elements'][0]['status']
-    if status == 'OK':
-        distance = [result['rows'][0]['elements'][0]['distance']['text'],{'m':result['rows'][0]['elements'][0]['distance']['value']}]
-        # with open("data.txt",'w') as file: # Use file to refer to the file object
-        #     file.write(str(distance[1]['m']))
-        # expected_time = [result['rows'][0]['elements'][0]['duration']['text'],{'s':result['rows'][0]['elements'][0]['duration']['value']}]
-        return True if float(distance[1]['m']) <= float(helping_radius*1000) else False 
-
-def find_smallest_dog(list_of_dogs):
-    size_values = {'S':1,'M':2,'B':3}
-    smallest_dog = 'B'
-    for dog in list_of_dogs:
-        if size_values[dog.size] < size_values[smallest_dog]:
-            smallest_dog = dog.size
-    return smallest_dog
-
-def check_dog_size(temp_user_dog, helper_max_size_dog):
-    size_values = {'S':1,'M':2,'B':3}
-    return True if size_values[temp_user_dog] <= size_values[helper_max_size_dog] else False
-
-
-def check_time(helper, time_period_id):
-
-    helper_time_period = TimePeriod.objects.all().filter( person_id = helper.user_id ).filter(time_type = 'F')
-    needy_time_period = TimePeriod.objects.all().filter( id__in = time_period_id ).filter(time_type = 'F')
-
-    data = []
-    for helper_time in helper_time_period:
-        for needy_time in needy_time_period:
-            if (helper_time.day == needy_time.day) and (helper_time.start_hour == needy_time.start_hour ) and (helper_time.end_hour == needy_time.end_hour):
-                # data.append({'helper':helper_time,'needy':needy_time})
-                data.append(helper_time)
-    return data
-
-
+from .matchPeople import matchUsers
 
 def chat(request):
-    data = []
-    temp_user = Profile.objects.all().filter(user = request.user)
-    temp_user_dogs = Dog.objects.all().filter(owner_id = temp_user[0].id)
-    temp_user_dogs_id = [ dog.id for dog in temp_user_dogs ] 
-
-    helpers = Profile.objects.all().filter(account_type='H')
-    list_of_helpers_distance = [ helpers[i] for i in range(helpers.count()) if check_location( temp_user[0].location, helpers[i].location, helpers[i].helping_radius) ]
-
-    dogs = DogTime.objects.all().filter(dog_id__in = temp_user_dogs_id).filter(match = False) 
-    
-    smallest_dog_size = find_smallest_dog(Dog.objects.all().filter( id__in =  dogs ))
-
-    time_period_id = [ dog.time_period_id for dog in dogs ]
-
-    list_of_helpers_time = [ ]
-    match_time_data = []
-    for helper in list_of_helpers_distance:
-        data = check_time( helper, time_period_id )
-        if len(data) != 0:
-            list_of_helpers_time.append(helper)
-            match_time_data.append(data)
-
-    list_of_helpers_dogs_size = [ helper for helper in list_of_helpers_time if check_dog_size(smallest_dog_size ,helper.max_dog_size )]
-    
-
-    return render(request, 'users/data.html',{'data':list_of_helpers_time,'match':match_time_data}) 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    matchUsers(request)
+    return render(request, 'users/data.html') 
 
 from datetime import datetime, timedelta, date
 from django.shortcuts import get_object_or_404
@@ -146,10 +56,9 @@ class CalendarView(generic.ListView):
         context['calendar'] = mark_safe(html_cal)
         return context
 
-from .utils import synchronize_with_google_calendar
+from .googleCalendarUtils import synchronize_with_google_calendar
 from django.http import HttpResponseRedirect
 
 def synchronize_calendar(request):
     synchronize_with_google_calendar(request)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-    #return render(request, 'users/calendar.html') 
